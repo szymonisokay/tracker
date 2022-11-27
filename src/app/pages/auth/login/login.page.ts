7c03b6b1c15b5d9b87b3f6726f8508497c3f80b9
Szymon Wałach <72652252+szymonisokay@users.noreply.github.com>
2022-11-27 19:45:50 +0100

    login and register page added

---
+++ src/app/pages/auth/login/login.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+})
+export class LoginPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/pages/auth/register/register.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+})
+export class RegisterPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
